9fecb0992d6dfc286992c3bd80903e0e8f561e2d
Mr-Harsh-Prasad <mrharshdrive@gmail.com>
2026-08-08 19:22:39 +0530

    billing

--- src/Application/controllers/BillingController.java
+++ src/Application/controllers/BillingController.java
@@ -1,4 +1,39 @@
 package Application.controllers;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.util.Duration;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class BillingController {
+    @FXML
+    private Label DateTimeLabel;
+
+    @FXML
+    public void initialize() {
+
+        Timeline timeline = new Timeline(
+                new KeyFrame(
+                        Duration.ZERO,
+                        e -> updateDateTime()
+                ),
+                new KeyFrame(Duration.seconds(1))
+        );
+
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+    }
+
+    private void updateDateTime() {
+        DateTimeFormatter formatter =
+                DateTimeFormatter.ofPattern("dd MMM yyyy | hh:mm:ss a");
+
+        DateTimeLabel.setText(
+                LocalDateTime.now().format(formatter)
+        );
+    }
 }
